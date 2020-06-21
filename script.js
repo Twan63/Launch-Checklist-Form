395d6cca8d1feded54238521d061eca34f9167cd
Antawan Ross <raelfilms@gmail.com>
2020-06-21 16:45:38 -0500

    HTMLCheckList

--- script.js
+++ script.js
@@ -11,3 +11,26 @@
 </ol>
 <img src="${}">
 */
+window.addEventListener("load", function(){
+      fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response){
+
+         response.json().then(function(json){
+
+            const div = document.getElementById("missionTarget");
+              let jIndex = Math.floor(Math.random()*json.length);
+                let planetData  = json[jIndex];
+              
+              
+              div.innerHTML = `
+              <h2>Mission Destination</h2>
+                  <ol>
+                  <li>Name: ${planetData.name}</li>
+                  <li>Diameter: ${planetData.diameter}</li>
+                  <li>Star: ${planetData.star}</li>
+                  <li>Distance from Earth: ${planetData.distance}</li>
+                  <li>Number of Moons: ${planetData.moons}</li>
+                  </ol>
+                  <img src="${planetData.image}">` ;
+          });
+      });
+});
